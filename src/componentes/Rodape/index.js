5db17bf5a68d5a7af72461cd294e5a40a1645211
EduardoDdSilva <128947051+EduardoDdSilva@users.noreply.github.com>
2024-02-08 23:28:23 -0300

    Add files via upload

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,17 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (
+        <section className='rodape'>
+            <div>
+                <img src="/imagens/fb.png" alt=""/>
+                <img src="/imagens/tw.png" alt=""/>
+                <img src="/imagens/ig.png" alt=""/>
+            </div>
+            <img src="/imagens/logo.png"/>
+            <p>Desenvolvido por Alura.</p>
+        </section>
+    )
+}
+
+export default Rodape
